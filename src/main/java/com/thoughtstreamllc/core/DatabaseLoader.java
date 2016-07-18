9ba3c68c3897f372ae10de213fee86de013737eb
JT Keller <jtkeller2@gmail.com>
2016-07-17 23:09:33 -0400

    Updating the data loader to generate several new courses and reviews.

--- src/main/java/com/thoughtstreamllc/core/DatabaseLoader.java
+++ src/main/java/com/thoughtstreamllc/core/DatabaseLoader.java
@@ -9,6 +9,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 @Component
@@ -24,6 +26,8 @@ public class DatabaseLoader implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         Course course = new Course("Java Basics", "https://teamtreehouse.com/library/java-basics");
         course.addReview(new Review(3, "You ARE a dork!!!"));
+        courses.save(course);
+
         String[] templates = {
                 "Up and Running with %s",
                 "%s Basics",
@@ -39,15 +43,17 @@ public class DatabaseLoader implements ApplicationRunner {
                 "Hibernate"
         };
 
+        List<Course> bunchOfCourses = new ArrayList<>();
         IntStream.range(0, 100)
                 .forEach(i -> {
                     String template = templates[i % templates.length];
                     String buzzword = buzzwords[i % buzzwords.length];
                     String title = String.format(template, buzzword);
                     Course c = new Course(title, "http://www.example.com");
-                    c.addReview(new Review(i % 5, String.format("Moar ")));
+                    c.addReview(new Review(i % 5, String.format("Moar %s please!!!", buzzword)));
+                    bunchOfCourses.add(c);
                 });
+        courses.save(bunchOfCourses);
 
-        courses.save(course);
     }
 }
